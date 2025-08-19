d0c350224efa7de1debd9834384588589e6bef58
wangyaohui <wyhsirius@gmail.com>
2025-08-19 11:36:47 +0800

    update

---
+++ dataset.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import random
+from PIL import Image
+from torch.utils.data import Dataset
+import glob
+import os
+from augmentations import AugmentationTransform
+from PIL import ImageFile
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+class Vox256_eval(Dataset):
+    def __init__(self, transform=None):
+        self.ds_path = './datasets/vox/test/'
+        self.videos = os.listdir(self.ds_path)
+        self.transform = transform
+
+    def __getitem__(self, idx):
+        vid_name = self.videos[idx]
+        video_path = os.path.join(self.ds_path, vid_name)
+
+        frames_paths = sorted(glob.glob(video_path + '/*.png'))
+        vid_target = [self.transform(Image.open(p).convert('RGB')) for p in frames_paths]
+
+        return vid_name, vid_target
+
+    def __len__(self):
+        return len(self.videos)
+
+
+class Taichi_eval(Dataset):
+    def __init__(self, transform=None):
+        self.ds_path = './datasets/taichi/test/'
+        self.videos = os.listdir(self.ds_path)
+        self.transform = transform
+
+    def __getitem__(self, idx):
+        vid_name = self.videos[idx]
+        video_path = os.path.join(self.ds_path, vid_name)
+
+        frames_paths = sorted(glob.glob(video_path + '/*.png'))
+        vid_target = [self.transform(Image.open(p).convert('RGB')) for p in frames_paths]
+
+        return vid_name, vid_target
+
+    def __len__(self):
+        return len(self.videos)
+
+
+class TED_eval(Dataset):
+    def __init__(self, transform=None):
+        self.ds_path = './datasets/ted/test/'
+        self.videos = os.listdir(self.ds_path)
+        self.transform = transform
+
+    def __getitem__(self, idx):
+        vid_name = self.videos[idx]
+        video_path = os.path.join(self.ds_path, vid_name)
+
+        frames_paths = sorted(glob.glob(video_path + '/*.png'))
+        vid_target = [self.transform(Image.open(p).convert('RGB')) for p in frames_paths]
+
+        return vid_name, vid_target
+
+    def __len__(self):
+        return len(self.videos)
