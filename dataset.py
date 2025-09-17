fa6c271dff5217cb6e514f43b4850e94bbab3fd8
Yaohui <wyhsirius@gmail.com>
2025-09-17 11:06:16 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,3 +1,13 @@
+# Copyright (C) 2021, Inria 
+# STARS research group, https://team.inria.fr/stars/
+# All rights reserved.
+#
+# This software is free for non-commercial, research and evaluation use
+# under the terms of the LICENSE.md file.
+#
+# For inquiries contact wyhsirius@gmail.com, francois.bremond@inria.fr, antitza.dantcheva@inria.fr
+#
+
 import pandas as pd
 import random
 from PIL import Image
